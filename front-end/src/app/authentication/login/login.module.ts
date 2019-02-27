826ab99f73fd6d706942500e44f717eb64e8069e
EL OUFIR Hatim <eloufirhatim@gmail.com>
2019-02-27 08:26:18 +0100

    Login page design

--- front-end/src/app/authentication/login/login.module.ts
+++ front-end/src/app/authentication/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material';
 
 const routes: Routes = [
   {
@@ -15,7 +16,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
 
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+
+    MatIconModule
   ]
 })
 export class LoginModule { }
